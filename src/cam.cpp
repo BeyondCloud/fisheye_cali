f6013ac03b31df2acb62616a011de28f0184b9b6
BeyondCloud <a1989796@yahoo.com.tw>
2016-07-22 18:50:06 +0800

    rewrite record_valid_rows as function

--- src/cam.cpp
+++ src/cam.cpp
@@ -13,33 +13,29 @@ using namespace std;
 using namespace cv;
 #define MIN_WIDTH 4
 #define KEY 25
-int threshold_value = 58;
+#define CLIP_WIDTH 886
+int threshold_value = 120;
 int threshold_type = 0;
 int const max_value = 255;
 int const max_type = 4;
 int const max_BINARY_value = 255;
-int main(int argc, const char** argv)
+struct note_t
 {
-    Mat Img;
-    Img = imread("line.jpg");
-    cvtColor(Img ,Img, COLOR_RGB2GRAY);
-    threshold( Img,Img, threshold_value, max_BINARY_value,threshold_type );
-
-
+    int tone = -1;
+    int bend = -1;
+};
+template<typename T, size_t N>
+void record_valid_rows(Mat &Img,T (&recorded_y)[N])
+{
     int key_cnt = 0;
     int pixel_cnt = 0;
     bool bw_switch = 0; //  black/white  true/false
-    bool recorded_y[Img.rows] = {false};
-    int bcnt  =0;
-    int wcnt  =0;
-
     for (int  y = 0; y <Img.rows ; y++)
     {
         for (int  x = 0; x <Img.cols ; x++)
         {
             if((int)Img.at<uchar>(y,x)==0)
             {
-                wcnt++;
                 if(bw_switch == true)
                 {
                     pixel_cnt = 0;
@@ -48,7 +44,6 @@ int main(int argc, const char** argv)
             }
             else
             {
-                bcnt++;
                 if(bw_switch == false)
                 {
                     pixel_cnt = 0;
@@ -62,7 +57,6 @@ int main(int argc, const char** argv)
             }
 
         }
-         //    cout<<key_cnt<<" ";
         if(key_cnt == KEY)
         {
             recorded_y[y] = true;
@@ -70,13 +64,40 @@ int main(int argc, const char** argv)
         key_cnt = 0;
         pixel_cnt = 0;
     }
+}
+int main(int argc, const char** argv)
+{
+    Mat Img;
+    Img = imread("line.jpg");
+    cvtColor(Img ,Img, COLOR_RGB2GRAY);
+    threshold( Img,Img, threshold_value, max_BINARY_value,threshold_type );
+    imshow("thres",Img);
+
+    bool recorded_y[CLIP_WIDTH] = {false};
+    record_valid_rows(Img,recorded_y);
+    note_t note[Img.rows][KEY];
+
+
+
 
 //cout<<"b"<<bcnt<<"w"<<wcnt;
+    cvtColor(Img,Img,COLOR_GRAY2BGR);
+
     for (int  y = 0; y <Img.rows ; y++)
     {
-        if( recorded_y[y])
-            cout<<y<<"\t";
+        if(recorded_y[y])
+        {
+            for (int  x = 0; x <Img.cols ; x++)
+            {
+
+                Img.at<Vec3b>(Point(x, y))[0] = 0;
+                Img.at<Vec3b>(Point(x, y))[1] = 0;
+                Img.at<Vec3b>(Point(x, y))[2] = 255;
+            }
+
+        }
     }
+
     imshow("asdf",Img);
     waitKey();
     return 0;
